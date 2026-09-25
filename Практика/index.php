94132bd114aea7aabd494cceeb27fdc129cca06b
Maria <marunua2008@gmail.com>
2026-09-25 14:46:05 +0300

    Практическая

--- Практика/index.php
+++ Практика/index.php
@@ -1,2 +1,57 @@
-<?php
-echo 'hello';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta  name="viewport" content="width=devic width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h1> Практическая 1</h1>
+    <h2> Формула 1</h2>
+    <p> Переменные</p>
+    <?php
+    $a= 6;
+    $b= 8;
+    $c= 12;
+    $d= 5;
+    $res = ($a / $b / $d) - ($a * $b - $c) / ($c * $d);
+    echo "a = $a<br>";
+    echo "b = $b<br>";
+    echo "c = $c<br>";
+    echo "d = $d<br>";
+    echo "Результат = $res";
+    ?>
+
+    <h2> Формула 2</h2>
+    <p>Переменные</p>
+    <?php
+    $x= 26;
+    $y= 43;
+    echo "x = $x<br>";
+    echo "y = $y<br>";
+    $res2 = ($x + $y) / ($y + 1) - ($x * $y - 12 ) / (34 + $x); 
+    echo "Результат = $res2";
+    ?> 
+
+    <h2> Формула 3</h2>
+    <p> Переменные</p>
+    <?php
+    $x = 5;
+    $y = 32;
+    echo "x = $x<br>";
+    echo "y = $y<br>";
+    $res3 = (($x + 1) / ($x - 1)) **$x  + 18 *  $x * $y**2;
+    echo "Результат = $res3";  
+    ?>  
+     <h2> Формула 4</h2>
+    <p>Переменные</p>
+    <?php
+    $x= 26;
+    $y= 43;
+    echo "x = $x<br>";
+    echo "y = $y<br>";
+    $res4 = ((1  + 1 / ($x**2)))** $x - 12 * ($x**2 * $y);
+    echo "Результат = $res3";  
+    ?>  
+</body>
+</html>
